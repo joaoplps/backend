1aa4cc1ec557825a6d4f95503e43831dfbedb91c
João Pedro LPS <38426476+joaoplps@users.noreply.github.com>
2018-08-17 15:17:56 -0300

    Update 100poo.java

--- POO/Level1poo/100poo.java
+++ POO/Level1poo/100poo.java
@@ -1,7 +1,8 @@
 package pkg101poo;			//Pacote referente ao projeto (diretório 101poo/src/pkg101poo)
 /**
  * Os códigos funcionarâo uma vez que a árvore de arquivos e diretórios do projeto tenham a nomenclatura correta.
- * Para isso crie um projeto com o nome 101poo e coloque os códigos no diretório descrito acima.
+ * Para isso crie um projeto com o nome 101poo e coloque os códigos no diretório descrito acima, um de cada vez,
+ * pois são da classe Main.
  * @author João Pedro LPS
  * Estrutura básica de um código Java.
  */
