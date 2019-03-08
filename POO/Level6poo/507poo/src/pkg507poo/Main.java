4afd9898dd63591c8901e465207c15862d3a16e8
jp <joao.pedro.pacheco@hotmail.com>
2019-03-08 16:40:23 -0300

    Still 507poo

--- POO/Level6poo/507poo/src/pkg507poo/Main.java
+++ POO/Level6poo/507poo/src/pkg507poo/Main.java
@@ -4,6 +4,7 @@ import Cards.Card;
 import Cards.ConvCard;
 import Cards.PifCard;
 import Cards.TrucoCard;
+import java.util.ArrayList;
 
 /**
  * Truco!
@@ -18,11 +19,23 @@ public class Main {
         Card c1 = new ConvCard(1, 5);
         Card c2 = new TrucoCard(2, 9);
 
+        //True:
         System.out.println(c2.equals(c2));
+        //False:
         System.out.println(c2.equals(c1));
         
         Card c3 = new PifCard(2, 8);
         
+        //False:
         System.out.println(c3.equals(c2));
+        
+        //ArrayList review:
+        ArrayList<Card> cardList = new ArrayList();
+        
+        Card c = new TrucoCard(1, 2);
+        cardList.add(c);
+        
+        //True:
+        System.out.println(cardList.contains(c));
     }
 }
