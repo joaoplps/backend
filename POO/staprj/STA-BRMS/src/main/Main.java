89f67bcb9766f70629d6255566010ff364e27ba6
JPLPS <joao.pedro.pacheco@hotmail.com>
2019-04-07 15:30:31 -0300

    Started UMLs

---
+++ POO/staprj/STA-BRMS/src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+/**
+ *
+ * @author PGS @LPS
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
